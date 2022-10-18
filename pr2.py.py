087c6e4425a3f9a176ee58523e861c42436484b3
aeshabalar <104067440+aeshabalar@users.noreply.github.com>
2022-10-19 05:23:30 +1200

    pr2.2.py

---
+++ pr2.py.py
@@ -0,0 +1,13 @@
+import email
+from unicodedata import name
+
+
+name=input("enter your name:")
+dob=input("enter your dob:")
+contact_no=input("enter your contact_no:")
+email_id=input("enter your email_id:")
+print("your name is:",name)
+print("your date of birth is:",dob)
+print("your contact_no is:",contact_no)
+print("your email_id is:",email_id)
+
