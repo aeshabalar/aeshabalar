802c36c9958fc7eb883950d79570b02c0efee77b
aeshabalar <104067440+aeshabalar@users.noreply.github.com>
2022-10-19 05:46:55 +1200

    Add files via upload
    to find a maximum of given three numbers (use ternary operator).

---
+++ pr3-4.py.py
@@ -0,0 +1,17 @@
+from sunau import AUDIO_FILE_ENCODING_DOUBLE
+
+
+A=int(input("enter value of A:"))
+B=int(input("enter value of B:"))
+C=int(input("enter value of C:"))
+if A>B and A>=C:
+    print("A is greatest",A)
+elif B>=A and B>=C:
+    print("B is greatest",B)
+else:
+    print("C is greatest",C)
+        
+
+
+
+
